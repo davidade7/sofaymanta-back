b55af6609e75c3a6444ab8d302ae4408af125eb8
David Perso <david.ade.1989@gmail.com>
2025-05-28 19:52:35 +0200

    fix: small change to see if its working on railway

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
     next();
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 
 bootstrap();
