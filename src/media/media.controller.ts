debd0752179315ba4acfcecf8e9e94b6a96e2c66
David Perso <david.ade.1989@gmail.com>
2025-06-01 18:32:31 +0200

    fix: some routes adjustments

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ParseIntPipe } from '@nestjs/common';
 import { TmdbService } from './media.service';
 
-@Controller('movies')
+@Controller('media')
 export class TmdbController {
   constructor(private readonly tmdbService: TmdbService) {}
 
-  @Get('recent')
+  @Get('movies/recent')
   async getRecentMovies(@Query('lang') lang: string): Promise<any> {
     return this.tmdbService.getRecentMovies(lang || 'es-ES');
   }
 
-  @Get('detail/:id')
+  @Get('movies/detail/:id')
   async getMovieDetails(
     @Param('id', ParseIntPipe) id: number,
     @Query('lang') lang: string,
