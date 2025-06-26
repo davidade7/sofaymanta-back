80c6c55fd7be27c816dba0a4043da628d9955958
David Perso <david.ade.1989@gmail.com>
2025-06-26 19:41:12 +0200

    fix: fix an error of import

--- src/user-media-interactions/user-media-interactions.module.ts
+++ src/user-media-interactions/user-media-interactions.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserMediaInteractionsService } from './user-media-interactions.service';
 import { UserMediaInteractionsController } from './user-media-interactions.controller';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { MediaModule } from '../media/media.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, forwardRef(() => MediaModule)],
   controllers: [UserMediaInteractionsController],
   providers: [UserMediaInteractionsService],
   exports: [UserMediaInteractionsService],
